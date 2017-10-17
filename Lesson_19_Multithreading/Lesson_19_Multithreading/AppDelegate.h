87b9f87b2ae09abd0aa87c1bfc1135d0d9a4c2dd
Andrey Proskurin <andrewproskurin@gmail.com>
2017-10-17 19:42:40 +0300

    Empty single view app has been added

---
+++ Lesson_19_Multithreading/Lesson_19_Multithreading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_19_Multithreading
+//
+//  Created by Andrey Proskurin on 17.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_19_Multithreading/Lesson_19_Multithreading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_19_Multithreading
+//
+//  Created by Andrey Proskurin on 17.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
